0c0bb214b7083fbbbeec201a9be6f70ecbf50c55
JGtm <g.sit@hotmail.fr>
2012-11-19 16:12:34 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<title>Home</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+    </head>
+    <body>
+	<div class="header">
+	    <div class="header-top">
+		<div class="header-logo"></div>
+		<div class="header-contact">
+		    <span>Any Question ?</span>
+		    <span class="phone"> Call Us: +1 800 559 6580</span>
+		</div>
+		<div class="nav"></div>
+	    </div>
+	</div>
+    </body>
+</html>
