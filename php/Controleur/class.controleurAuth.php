c030d5a2a55c9a15e115ff7db03f2a23ef92461c
leonetsky <leo.georgel@gmail.com>
2012-11-21 10:22:03 +0100

    controleur bonus test Operationel

--- php/Controleur/class.controleurAuth.php
+++ php/Controleur/class.controleurAuth.php
@@ -1,10 +1,9 @@
 <?php
 
-
-class Controleur_Formulaire 
+class Controleur_Formulaire
 {
-//put your code here
 
+//put your code here
     /////////////////////////////////////////////
     //Constructeur///////////////////////////////
     /////////////////////////////////////////////
@@ -13,33 +12,39 @@ class Controleur_Formulaire
         
     }
 
-    
     /////////////////////////////////////////////
     //fonctions//////////////////////////////////
     /////////////////////////////////////////////
-    
-    public function validFormulaireAuth()
+
+    public function validFormulaireAuth($leMotdePasse)
     {
       
     }
-    
-    
+
     public function genererFormulaireAuth()
     {
-         $formulaireAuth='';
-         $formulaireAuth.= '<form method="POST" action="#">';  
-         $formulaireAuth.= '<label>e-Mail : </label>';
-         $formulaireAuth.= '<input type="text" name="email" value="" />';
-         $formulaireAuth.= '<br />';
-         $formulaireAuth.= '<label>Mot de passe : </label>';
-         $formulaireAuth.= '<input type="password" name="mdp" value="" />';
-         $formulaireAuth.= '<br />';
-         $formulaireAuth.= '<input type="submit" name="valider" value="Valider" />';
-         $formulaireAuth.= '</form>';
-         
-         echo $formulaireAuth;
-    }
+        $formulaireAuth = '';
+        $formulaireAuth.='<form method="POST" action="#">';
+        $formulaireAuth.='<table border = "0">';
+        $formulaireAuth.='<tbody>';
+        $formulaireAuth.='<tr>';
+        $formulaireAuth.='<td><label>email : </label></td>';
+        $formulaireAuth.='<td><input type="text" name="email" value="" /></td>';
+        $formulaireAuth.='</tr>';
+        $formulaireAuth.='<tr>';
+        $formulaireAuth.='<td><label>Mot de passe : </label></td>';
+        $formulaireAuth.='<td><input type="password" name="mdp" value="" /></td>';
+        $formulaireAuth.='</tr>';
+        $formulaireAuth.='<tr>';
+        $formulaireAuth.='<td><td align="right">';
+        $formulaireAuth.='<input type="submit" name="valider" value="Valider"  />';
+        $formulaireAuth.='</td>';
+        $formulaireAuth.='</tr>';
+        $formulaireAuth.='</tbody>';
+        $formulaireAuth.='</table>';
+        $formulaireAuth.='</form>';
 
-    
+        return $formulaireAuth;
+    }
 }
 ?>
--- php/Controleur/class.controleurMain.php
+++ php/Controleur/class.controleurMain.php
@@ -1,17 +1,25 @@
 <?php
 require_once 'class.controleurAuth.php';
+require_once 'class.dbb.php';
 
 class Controleur_Main
 {
+ 
     private $controleurSpe;
+    private $bdd;
   
     function __construct($page)
     {
+        $bdd=new Modele_bdd();
+        
         switch ($page)
         {
             case 'authentification':
                 $this->controleurSpe = new Controleur_Formulaire();
-                $this->controleurSpe->genererFormulaireAuth();
+                echo $this->controleurSpe->genererFormulaireAuth();
+                
+                $acn=$bdd->seConnecter();
+                $bdd->selectionner($acn, 'utilisateur', 'mdp', $_SESSION['mdp']);
                 
         }
     }
